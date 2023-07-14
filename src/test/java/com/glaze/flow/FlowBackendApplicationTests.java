096170adc584135639f74bfdac7fac29288f43d5
Santiago <glaze@outlook.es>
2023-07-14 18:36:11 -0500

    chore: first commit

---
+++ src/test/java/com/glaze/flow/FlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glaze.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
