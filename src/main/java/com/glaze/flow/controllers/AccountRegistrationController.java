b84746154e0c1722dfccca15bebf658c1936bc24
Santiago <glaze@outlook.es>
2023-07-17 20:21:25 -0500

    feat: send email
    - send email with active account link on account creation
    - fixes user details not being created on account creation
    - activates account from email

--- src/main/java/com/glaze/flow/controllers/AccountRegistrationController.java
+++ src/main/java/com/glaze/flow/controllers/AccountRegistrationController.java
@@ -1,20 +1,22 @@
 package com.glaze.flow.controllers;
 
+import com.glaze.flow.services.AccountVerificationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/v1/account")
+@RequiredArgsConstructor
 public class AccountRegistrationController {
+    private final AccountVerificationService accountVerificationService;
 
-    @GetMapping("/account-registration")
-    public String save(
-        @RequestParam(defaultValue = "Glaze") String name,
-        Model modeal
-    ) {
-        modeal.addAttribute("username", name);
-        return "/account-registration-email-sent";
+    @GetMapping("/account-activation/{userId}")
+    public String activateAccount(@PathVariable Long userId) {
+        accountVerificationService.activateAccount(userId);
+        return "dummy";
     }
 
 }
--- src/main/java/com/glaze/flow/repositories/AccountVerificationTokenRepository.java
+++ src/main/java/com/glaze/flow/repositories/AccountVerificationTokenRepository.java
@@ -3,4 +3,5 @@ package com.glaze.flow.repositories;
 import com.glaze.flow.entities.AccountVerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountVerificationTokenRepository extends JpaRepository<AccountVerificationToken, Long> {}
+public interface AccountVerificationTokenRepository
+    extends JpaRepository<AccountVerificationToken, Long> {}
