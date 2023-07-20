6bde15ced09e8730b5b89e1c51f26352589f3b83
Santiago <glaze@outlook.es>
2023-07-20 18:22:07 -0500

    refactor: decouple userservice from accountservice and email service with events

--- src/main/java/com/glaze/flow/controllers/UserController.java
+++ src/main/java/com/glaze/flow/controllers/UserController.java
@@ -4,6 +4,8 @@ import com.glaze.flow.dtos.request.SignUpRequest;
 import com.glaze.flow.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @PostMapping("/account-registration")
     public ResponseEntity<Long> save(@Valid @RequestBody SignUpRequest request) {
+        LOGGER.info("User registration endpoint reached with request {}", request);
         Long savedId = userService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED)
